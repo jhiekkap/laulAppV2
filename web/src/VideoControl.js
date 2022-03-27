4a5ad0a43e6a4c6a738c7caffadd27de1ac52e1c
jhiekkap <jari.hiekkapelto@gmail.com>
2022-03-27 20:43:43 +0300

    useRef

--- web/src/VideoControl.js
+++ web/src/VideoControl.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
-import "./VideoControl.css"; 
+import React, { useState, useEffect, useRef } from "react";
+import "./VideoControl.css";
 import axios from "axios";
 
 //let recordingTimeMS = 5000;
-const apiBaseUrl = "http://localhost:3001"; 
+const apiBaseUrl = "http://localhost:3001";
 
-function VideoControl() {
+const VideoControl = () => {
   const [videoMode, setVideoMode] = useState("recording");
   const [downloadButton, setDownloadButton] = useState({});
   const [users, setUsers] = useState([]);
@@ -16,50 +16,60 @@ function VideoControl() {
   const [watchProduction, setWatchProduction] = useState("");
   const [watchTrack, setWatchTrack] = useState("");
 
+  const previewRef = useRef();
+  const recordingRef = useRef();
+  const playRemoteRef = useRef(); 
+
   const chosenVideoTrack = watchTrack
-    ? productions.find((production) => production.id == watchProduction).name +
+    ? productions.find((production) => production.id === watchProduction).name +
       "/" +
-      tracks.find((track) => track.id == watchTrack).name
+      tracks.find((track) => track.id === watchTrack).name
     : "";
 
   const fetchData = async () => {
-    const { data: users } = await axios.get(apiBaseUrl + "/api/users");
-    setUsers(users);
-    const { data: productions } = await axios.get(
-      apiBaseUrl + "/api/productions"
-    );
-    setProductions(productions);
-    const { data: tracks } = await axios.get(apiBaseUrl + "/api/tracks");
-    setTracks(tracks);
+    try{
+      const { data: users } = await axios.get(apiBaseUrl + "/api/users");
+      setUsers(users);
+      const { data: productions } = await axios.get(
+        apiBaseUrl + "/api/productions"
+      );
+      setProductions(productions);
+      const { data: tracks } = await axios.get(apiBaseUrl + "/api/tracks");
+      setTracks(tracks);
+    } catch (error) {
+      console.log(error);
+    } 
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  console.log(
-    "USERS:",
-    users,
-    "PRODUCTIONS: ",
-    productions,
-    "TRACKS: ",
-    tracks
-  );
-  console.log("TRACKINFO", trackInfo);
-  console.log("WATCH PRODUCTION & TRACK", watchProduction, watchTrack);
-
+  // console.log(
+  //   "USERS:",
+  //   users,
+  //   "PRODUCTIONS: ",
+  //   productions,
+  //   "TRACKS: ",
+  //   tracks
+  // );
+  // console.log("TRACKINFO", trackInfo);
+  // console.log("WATCH PRODUCTION & TRACK", watchProduction, watchTrack);
 
   /* function wait(delayInMS) {
     return new Promise(resolve => setTimeout(resolve, delayInMS));
   } */
 
-  function startRecording(stream, lengthInMS) {
+  const startRecording = (stream, lengthInMS) => {
     let recorder = new MediaRecorder(stream);
     let data = [];
 
-    recorder.ondataavailable = (event) => data.push(event.data);
+    recorder.ondataavailable = (event) => {
+      console.log(event.data, Date.now());
+      data.push(event.data);
+    };
     recorder.start();
-   console.log(recorder.state + " for " + lengthInMS / 1000 + " seconds...");
+    // console.log(recorder.state + " for " + lengthInMS / 1000 + " seconds...");
 
     let stopped = new Promise((resolve, reject) => {
       recorder.onstop = resolve;
@@ -67,29 +77,29 @@ function VideoControl() {
     });
 
     /* let recorded = wait(lengthInMS).then(
-      () => recorder.state == "recording" && recorder.stop()
+      () => recorder.state === "recording" && recorder.stop()
     ); */
 
     return Promise.all([
       stopped,
       // recorded
     ]).then(() => data);
-  }
+  };
 
-  function stop(stream) {
+  const stop = (stream) => {
     stream.getTracks().forEach((track) => track.stop());
-  }
+  };
 
   const handleStartButton = () => {
     setVideoMode("recording");
-    let preview = document.getElementById("preview");
+    const preview = previewRef.current;
     navigator.mediaDevices
       .getUserMedia({
         video: true,
         audio: true,
       })
       .then((stream) => {
-        console.log("STREAM", stream);
+        // console.log("STREAM", stream);
         preview.srcObject = stream;
         setDownloadButton({ ...downloadButton, href: stream });
         preview.captureStream =
@@ -101,89 +111,94 @@ function VideoControl() {
       )
       .then((recordedChunks) => {
         let recordedBlob = new Blob(recordedChunks, { type: "video/mp4" });
-        console.log("BLOBBI", recordedBlob);
-        setBlob(recordedBlob);
-        let recording = document.getElementById("recording");
+        // console.log("BLOBBI", recordedBlob);
+        setBlob(recordedBlob); 
+        const recording = recordingRef.current;
         recording.src = URL.createObjectURL(recordedBlob);
         setDownloadButton({
           download: "RecordedVideo.webm",
           href: recording.src,
         });
 
-       console.log(
-          "Successfully recorded " +
-            recordedBlob.size +
-            " bytes of " +
-            recordedBlob.type +
-            " media."
-        );
+        // console.log(
+        //   "Successfully recorded " +
+        //     recordedBlob.size +
+        //     " bytes of " +
+        //     recordedBlob.type +
+        //     " media."
+        // );
       })
       .catch(console.log);
   };
   const handleStopButton = () => {
-    let preview = document.getElementById("preview");
+    const preview = previewRef.current;
     stop(preview.srcObject);
     setVideoMode("playLocal");
   };
 
   const handleUploadButton = async () => {
-    console.log("UPLOADING", downloadButton);
+    // console.log("UPLOADING", downloadButton);
+
     const name =
-      productions.find((production) => production.id == trackInfo.productionId)
+      productions.find((production) => production.id === trackInfo.productionId)
         .name +
       "_" +
       trackInfo.trackName;
+
     const file = new File([blob], name, {
       lastModified: new Date(),
       type: "video/webm",
     });
-    console.log("UUSI FILEE", file);
+    // console.log("UUSI FILEE", file);
+
     const formData = new FormData();
     formData.append("file", file);
 
-    let options = {
-      url: apiBaseUrl + "/api/files",
-      method: "POST",
-      data: formData,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "multipart/form-data",
-      },
-    };
+    try {
+      const { data: newFile } = await axios({
+        url: apiBaseUrl + "/api/files",
+        method: "POST",
+        data: formData,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("NEW FILE", newFile.data);
 
-    const { data: newFile } = await axios(options).catch(console.log);
-    console.log("NEW FILE", newFile.data);
-    const { data: newTrack } = await axios
-      .post(apiBaseUrl + "/api/tracks", {
+      const { data: newTrack } = await axios.post(apiBaseUrl + "/api/tracks", {
         url: newFile.data.url,
         userId: "1234",
         productionId: trackInfo.productionId,
         name: trackInfo.trackName,
-      })
-      .catch(console.log);
-    console.log("NEW TRACK", newTrack);
-    setTracks(tracks.concat(newTrack));
+      });
+
+      // console.log("NEW TRACK", newTrack);
+      setTracks(tracks.concat(newTrack));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handlePlayVideo = () => {
     console.log("PLAYING VIDEO");
     setVideoMode("playRemote");
-    let playRemote = document.getElementById("playRemote");
+    const playRemote = playRemoteRef.current;
     playRemote.src =
       "http://localhost:3001" +
-      tracks.find((track) => track.id == watchTrack).url;
+      tracks.find((track) => track.id === watchTrack).url;
   };
 
   return (
     <div className="block videoBlock">
       <div style={{ display: videoMode === "recording" ? "block" : "none" }}>
-        <video id="preview" width="270" height="200" autoPlay muted></video>
+        <video ref={previewRef} id="preview" width="270" height="200" autoPlay muted></video>
       </div>
       <div style={{ display: videoMode === "playLocal" ? "block" : "none" }}>
-        <video id="recording" width="270" height="200" controls></video>
+        <video ref={recordingRef} id="recording" width="270" height="200" controls></video>
       </div>
       <div style={{ display: videoMode === "playRemote" ? "block" : "none" }}>
-        <video id="playRemote" width="270" height="200" autoPlay></video>
+        <video ref={playRemoteRef} id="playRemote" width="270" height="200" autoPlay></video>
       </div>
       <div className="videoButtons">
         <button onClick={() => handleStartButton()}>REC</button>
@@ -194,7 +209,7 @@ function VideoControl() {
             href={downloadButton.href}
             download={downloadButton.download}
           >
-            DOWNLOAD{" "}
+            DOWNLOAD
           </a>
         </button>
       </div>
@@ -232,7 +247,9 @@ function VideoControl() {
         </button>
       </div>
       {/* <div id="divider"></div> */}
-      <br/><br/><br/>
+      <br />
+      <br />
+      <br />
       PLAYBACK
       <div className="titleRow">
         UPLOADED SONGS
@@ -257,7 +274,7 @@ function VideoControl() {
           >
             <option>valitse</option>
             {tracks
-              .filter((track) => track.productionId == watchProduction)
+              .filter((track) => track.productionId === watchProduction)
               .map((track) => (
                 <option key={track.id} value={track.id}>
                   {track.name}
@@ -266,13 +283,13 @@ function VideoControl() {
           </select>
           {watchTrack && (
             <button type="button" onClick={handlePlayVideo}>
-              PLAY {chosenVideoTrack}{" "}
+              PLAY {chosenVideoTrack}
             </button>
           )}
         </div>
       )}
     </div>
   );
-}
+};
 
 export default VideoControl;
